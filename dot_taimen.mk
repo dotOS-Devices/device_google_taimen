d34d05e7d487a6af8188726ddd464614620013e0
PixelBoot <bootingpixels@gmail.com>
2022-09-19 04:39:54 -0700

    taimen: convert to dot tree

---
+++ dot_taimen.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Inherit some common Dot stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := dot_taimen
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2 XL
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=taimen \
+    PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
+
+BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
